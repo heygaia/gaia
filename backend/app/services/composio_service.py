78b1e23169ef07eb77f3f4f0d432606c627618b8
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-08-18 13:58:22 +0530

    refactor: composio tool streaming ui builder

--- backend/app/services/composio_service.py
+++ backend/app/services/composio_service.py
@@ -1,9 +1,9 @@
 from app.config.loggers import app_logger as logger
 from app.config.settings import settings
 from app.services.langchain_composio_service import LangchainProvider
-from composio import Composio, before_execute, after_execute
+from composio import Composio, before_execute
 from composio.types import ToolExecuteParams,ToolExecutionResponse
-from langgraph.config import get_stream_writer
+from app.utils.tool_ui_builders import frontend_stream_modifier
 
 SOCIAL_CONFIGS = {
     "notion": {"auth_config_id": "ac_9Yho1TxOcxHh", "toolkit": "NOTION"},
@@ -43,25 +43,6 @@ def extract_user_id_from_params(
     params["user_id"] = user_id
     return params
 
-def after_execute_modifier(
-    tool: str,
-    toolkit: str,
-    params: ToolExecuteParams,
-
-) -> ToolExecutionResponse:
-    print("running the tool executing after")
-    email_data = [
-            {
-                "to": [params["arguments"]["recipient_email"]],
-                "subject": params["arguments"]["subject"],
-                "body": params["arguments"]["body"],
-            }
-        ]
-    writer = get_stream_writer()
-    writer({"email_compose_data": email_data})
-    print("this is params",params)
-    return params
-
 
 class ComposioService:
     def __init__(self):
@@ -98,15 +79,13 @@ class ComposioService:
             toolkits=[tool_kit],
         )
         tools_name = [tool.name for tool in tools]
-        # Applying the before_execute decorator dynamically
         user_id_modifier = before_execute(tools=tools_name)(extract_user_id_from_params)
-        after_modifier = before_execute(tools=["GMAIL_CREATE_EMAIL_DRAFT"])(after_execute_modifier)
+        after_modifier = before_execute(tools=tools_name)(frontend_stream_modifier)
 
         return self.composio.tools.get(
             user_id="",
             toolkits=[tool_kit],
             modifiers=[after_modifier, user_id_modifier],
         )
-
 
 composio_service = ComposioService()
