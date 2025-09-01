2e9043c0022ae754ab585a26644ced92837686e5
Aryan <aryan.k.randeriya@gmail.com>
2025-09-02 03:30:09 +0530

    chore: Update commitlint configuration to include subject-case rules

--- frontend/commitlint.config.js
+++ frontend/commitlint.config.js
@@ -1,1 +1,6 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "subject-case": [0, "always", ["sentence-case", "start-case", "pascal-case", "upper-case", "lower-case"]],
+  },
+};
