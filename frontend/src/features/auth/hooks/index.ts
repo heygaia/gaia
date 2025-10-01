8d4ed3d5dd8f98c3b09ae0884e3013bd5bf3c6a2
Aryan <aryan@heygaia.io>
2025-10-01 22:55:34 +0530

    feat: Implement custom logout hook to clear react query cache and stores after logging out

---
+++ frontend/src/features/auth/hooks/index.ts
@@ -0,0 +1,7 @@
+export * from "./useUser";
+export * from "./useLogout";
+export * from "./useFetchUser";
+export * from "./useLoginModal";
+export * from "./useOnboardingGuard";
+export * from "./handleGoogleLogin";
+export * from "./handleAuthLogin";
